5172d872b6528bbf0a6fba6aeea4a3fe68cc292a
Nathaniel Finger <nfinger1020@gmail.com>
2018-05-15 12:20:14 -0400

    get rid of unnecessary

--- Game.py
+++ Game.py
@@ -1,9 +1,6 @@
 from flask_mongoengine import MongoEngine
 from flask_mongoengine.wtf import model_form
 import random
-import binascii
-from Crypto.Cipher import AES
-import base64
 db = MongoEngine()
 
 KEY = 'nate_is_the_best'
@@ -44,27 +41,6 @@ class Game(db.Document):
     def encrypt(key, iv, plaintext):
         arr = ["0x" + elem.encode("hex") for elem in plaintext]
         return arr
-
-    @staticmethod
-    def decrypt(key, iv, encrypted_text):
-        aes = AES.new(key, MODE, iv, segment_size=SEGMENT_SIZE)
-        encrypted_text_bytes = binascii.a2b_hex(encrypted_text)
-        decrypted_text = aes.decrypt(encrypted_text_bytes)
-        decrypted_text = Game._unpad_string(decrypted_text)
-        return decrypted_text
-
-    @staticmethod
-    def _pad_string(value):
-        length = len(value)
-        pad_size = BLOCK_SIZE - (length % BLOCK_SIZE)
-        return value.ljust(length + pad_size, '\x00')
-
-    @staticmethod
-    def _unpad_string(value):
-        while value[-1] == '\x00':
-            value = value[:-1]
-        return value
-
 
     @staticmethod
     def return_helper(game):
